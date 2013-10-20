4132eb8eb2679b9080626255477a9c5fff80e57d
Matt Kenney <mkenney@mjk-tech.com>
2013-10-20 16:35:33 -0400

    app using express

---
+++ app.js
@@ -0,0 +1,84 @@
+#!/usr/bin/env nodejs
+
+var credentials = require('./credentials'),
+    express = require('express'),
+    flash = require('connect-flash'),
+    swig = require('swig'),
+    app = express();
+
+// ***** Initialization *****
+
+app.enable('trust proxy');
+app.engine('html', swig.renderFile);
+
+app.set('view cache', false);
+app.set('view engine', 'html');
+app.set('views', __dirname + '/views');
+
+// ***** Middleware *****
+
+app.use(express.static('public'));
+app.use(express.bodyParser());
+app.use(express.cookieParser(credentials.cookie));
+app.use(express.cookieSession());
+app.use(express.logger());
+app.use(express.errorHandler());
+app.use(flash());
+
+// flash support -- we need to call req.flash() just before render()
+app.use(function(req, res, next)
+{
+    var render = res.render;
+    res.render = function (view, locals, callback)
+    {
+        if (typeof callback !== 'function' && typeof locals === 'function')
+        {
+            callback = locals;
+            locals = {};
+        }
+        else if (!locals)
+        {
+            locals = {};
+        }
+        locals.alerts = req.flash();
+        render.call(res, view, locals, callback);
+    };
+    next();
+});
+
+// ***** Controllers *****
+
+require('./controllers/api')(app);
+
+// ***** Error handling *****
+
+// if we get past all the controllers then we 404
+app.use(function (req, res, next)
+{
+    res.status(404);
+    res.render('404',
+    {
+        status: 404,
+        url: req.url,
+        user: req.user
+    });
+});
+
+// pretty error page
+app.use(function (err, req, res, next)
+{
+    res.status(500);
+    res.render('500',
+    {
+        status: err.status || 500,
+        error: err.message,
+        user: req.user
+    });
+});
+
+// ***** Server *****
+
+app.listen(3000);
+
+console.log('Listening on port 3000');
+
---
+++ controllers/api.js
@@ -0,0 +1,61 @@
+var weather = require('../lib/weather')
+,   locator = require('../lib/locator')
+,   xml2js = require('xml2js')
+;
+
+module.exports = function (app)
+{
+    app.get('/api/county', function (req, res)
+    {
+        locator.county(req.query, function (err, data)
+        {
+            if (err) return next(err);
+            res.send(data[0][0]);
+        });
+    });
+
+    app.get('/api/forecast', function (req, res, next)
+    {
+        locator.forecast(req.query, function (err, data)
+        {
+            if (err) return next(err);
+            weather(data, function (err, data)
+            {
+                if (err) return next(err);
+                res.send(data);
+            });
+        });
+    });
+
+    app.get('/api/observation', function (req, res, next)
+    {
+        locator.observation(req.query, 3, function (err, data)
+        {
+            if (err) return next(err);
+            weather(data, function (err, data)
+            {
+                if (err) return next(err);
+                res.send(data);
+            });
+        });
+    });
+
+    app.get('/api/place', function (req, res, next)
+    {
+        locator.place(req.query, function (err, data)
+        {
+            if (err) return next(err);
+            res.send(data[0][0]);
+        });
+    });
+
+    app.get('/api/radar', function (req, res, next)
+    {
+        locator.radar(req.query, function (err, data)
+        {
+            if (err) return next(err);
+            res.redirect(data[0][0].url);
+        });
+    });
+};
+
